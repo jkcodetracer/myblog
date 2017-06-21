9fb3a6ff180da973b3f0f4b9ac4ecb65f4df9330
codetracer <joker0910@sina.com>
2017-06-22 01:07:23 +1000

    extends get_context_data method to create paginator

--- blog/views.py
+++ blog/views.py
@@ -40,6 +40,62 @@ class IndexView(ListView):
     context_object_name = 'article_list'
     paginate_by = 5
 
+    def get_context_data(self, **kwargs):
+        # fetch the original context
+        context = super().get_context_data(**kwargs)
+
+        paginator = context.get('paginator')
+        page = context.get('page_obj')
+        is_paginated = context.get('is_paginated')
+
+        pagination_data = self.extra_pagination_data(paginator, page, is_paginated)
+
+        context.update(pagination_data)
+
+        return context
+
+    def extra_pagination_data(self, paginator, page, is_paginated):
+        if not is_paginated:
+            return {}
+
+        current_page = page.number
+        total_pages = paginator.num_pages
+        # [1,2,3,4,5] page index
+        page_range = paginator.page_range
+
+        left = []
+        right = []
+        left_has_more = False
+        right_has_more = False
+        first = False
+        last = False
+
+        right = page_range[current_page:
+                               (current_page+2) if (current_page+2) < total_pages else total_pages]
+        if len(right) > 0 and right[-1] < total_pages - 1:
+            right_has_more = True
+        if len(right) > 0 and right[-1] < total_pages:
+            last = True
+
+        left = page_range[(current_page-3) if (current_page-3) > 0 else 0:current_page-1]
+        if len(left) > 0 and left[0] > 2:
+            left_has_more = True
+        if len(left) > 0 and left[0] > 1:
+            first = True
+
+        context = {
+            'left':left,
+            'right':right,
+            'left_has_more':left_has_more,
+            'right_has_more':right_has_more,
+            'first':first,
+            'last':last,
+        }
+
+        return context
+
+
+
 def detail(request, pk):
     article = get_object_or_404(Article, pk=pk)
     # to create an instance of markdown
