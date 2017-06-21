0e383876237f192b3dbc72a6ce94027007940ecd
codetracer <joker0910@sina.com>
2017-06-21 13:59:22 +1000

    support markdown

--- blog/views.py
+++ blog/views.py
@@ -2,6 +2,8 @@ from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse
 from .models import Article
 
+# support markdown
+import markdown
 
 # Create your views here.
 def index(request):
@@ -12,5 +14,11 @@ def index(request):
 
 def detail(request, pk):
     article = get_object_or_404(Article, pk=pk)
+    article.body = markdown.markdown(article.body,
+                                     extensions=[
+                                         'markdown.extensions.extra',
+                                         'markdown.extensions.codehilite',
+                                         'markdown.extensions.toc',
+                                     ])
     return render(request, 'blog/detail.html', context={'post':article})
 
